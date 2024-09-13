3b1bd5534d3c35e6cb2b4dfae62e3ff0016c22ba
The Binh <abcmnpq028@gmail.com>
2024-09-13 15:03:11 +0700

    practice 7

--- USTHWeather/app/src/main/java/vn/edu/usth/weather/WeatherActivity.java
+++ USTHWeather/app/src/main/java/vn/edu/usth/weather/WeatherActivity.java
@@ -11,29 +11,31 @@ import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
+import androidx.annotation.NonNull;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentPagerAdapter;
+import androidx.viewpager.widget.ViewPager;
+
+import com.google.android.material.tabs.TabLayout;
+
 public class WeatherActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_main);
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
-            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
-            return insets;
-        });
+//        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
+//            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
+//            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
+//            return insets;
+        ViewPager viewPager = findViewById(R.id.view_pager);
+        TabLayout tabLayout = findViewById(R.id.tab_layout);
 
-        // creating and initializing variable for fragment transaction.
-        FragmentTransaction ft = getSupportFragmentManager().beginTransaction();
+        WeatherPagerAdapter pagerAdapter = new WeatherPagerAdapter(getSupportFragmentManager());
+        viewPager.setAdapter(pagerAdapter);
+        tabLayout.setupWithViewPager(viewPager);
 
-        Fragment fragment1 = new WeatherFragment();
-        ft.add(R.id.upperContainer, fragment1, "fragment1");
 
-        Fragment fragment2 = new aForecastFragment();
-        ft.add(R.id.lowerContainer, fragment2, "fragment2");
-
-        // committing the transaction.
-        ft.commit();
 //        aForecastFragment firstFragment = new aForecastFragment();
 //        getSupportFragmentManager().beginTransaction().add(R.id.forecast_frag, firstFragment).commit();
     }
@@ -45,24 +47,50 @@ public class WeatherActivity extends AppCompatActivity {
     }
 
     @Override
-    protected void onResume(){
+    protected void onResume() {
         super.onResume();
-        Log.i("Resume","Resume");
+        Log.i("Resume", "Resume");
     }
 
     @Override
-    protected void onPause(){
+    protected void onPause() {
         super.onPause();
-        Log.i("Pause","Pause");
+        Log.i("Pause", "Pause");
     }
+
     @Override
-    protected void onStop(){
+    protected void onStop() {
         super.onStop();
-        Log.i("Stop","Stop");
+        Log.i("Stop", "Stop");
     }
+
     @Override
-    protected void onDestroy(){
+    protected void onDestroy() {
         super.onDestroy();
-        Log.i("Destroy","Destroy");
+        Log.i("Destroy", "Destroy");
+    }
+
+    private class WeatherPagerAdapter extends FragmentPagerAdapter {
+        private final String[] tabTitles = new String[]{"HaNoi VietNam", "Paris France", "Toulouse France"};
+
+        public WeatherPagerAdapter(@NonNull FragmentManager fm) {
+            super(fm, BEHAVIOR_RESUME_ONLY_CURRENT_FRAGMENT);
+        }
+
+        @NonNull
+        @Override
+        public Fragment getItem(int position) {
+            return new WeatherAndForecastFragment();
+        }
+
+        @Override
+        public int getCount() {
+            return 3;
+        }
+
+        @Override
+        public CharSequence getPageTitle(int position) {
+            return tabTitles[position];
+        }
     }
 }
